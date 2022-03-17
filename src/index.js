70f5e4ab5b38122219972e9d33955590325c1a7f
Moabe <moabeluis2018@gmail.com>
2022-03-16 22:24:34 -0300

    Primeiro teste no insomia

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
-const { v4 }  = require("uudi")
+const { v4: uuidv4 }  = require("uuid")
 
 const app = express()
+
+app.use(express.json())
+
+const customers = []
 
 /**
  * cpf -string
@@ -11,6 +15,18 @@ const app = express()
  */
 app.post("/account", (request, response) =>{
     const { cpf, name } = request.body
+
+    const id = uuidv4()
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: [],
+    })
+
+    return response.status(201).send()
+
 })
 
 app.listen(3333)
